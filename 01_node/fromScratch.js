e1ad73989b47728b137fa13d2f31180aa30bfe49
Henrynunez112 <henrynunez@pursuit.org>
2020-03-21 11:32:54 -0400

    worked on the first and last part of linkedlist

--- 01_node/fromScratch.js
+++ 01_node/fromScratch.js
@@ -5,3 +5,24 @@
  * Create your own Node class from scratch to do this!
  * DO NOT import the Node class from ./node.js
  */
+
+
+ class Node {
+     constructor(val){
+        this.val = val,
+        this.next = null
+     }
+ }
+
+ const food = new Node('P');
+ const food2 = new Node('i');
+ const food3 = new Node('z');
+ const food4 = new Node('z');
+ const food5 = new Node('a');
+
+ food.next = food2;
+ food2.next = food3;
+ food3.next = food4;
+ food4.next = food5;
+
+ console.log(JSON.stringify(food))
--- 01_node/makeList.js
+++ 01_node/makeList.js
@@ -5,9 +5,24 @@ const { Node } = require('./node');
  * There are many ways you can do this!
  */
 const list = new Node('A');
+
+const name = new Node('H');
+const name2 = new Node('E');
+const name3 = new Node('N');
+const name4 = new Node('R');
+const name5 = new Node('Y');
+
+
+name.next = name2;
+name2.next = name3;
+name3.next = name4;
+name4.next = name5;
+
+
+
 // your code here
 
-console.log(JSON.stringify(list));
+console.log(JSON.stringify(name));
 
 /**
  * Create a linked list that spells your first or last name.
